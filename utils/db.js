cc7fd59a1d21b49998885b454142478932cc9c26
Chanuka Dilshan <mchanuka72@gmail.com>
2025-05-14 19:22:32 +0530

    connectDB change

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,11 @@
-// db.js (or where you handle DB connection)
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGO_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env");
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn; // Reuse existing connection
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      serverSelectionTimeoutMS: 30000, // 30s timeout (default: 10s)
-      socketTimeoutMS: 45000, // 45s timeout
-    }).then(mongoose => mongoose);
-  }
-
+export const connectDB = async () => {
   try {
-    cached.conn = await cached.promise;
-    console.log("MongoDB Connected!");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected" + conn.connection.host);
   } catch (err) {
-    console.error("MongoDB Connection Error:", err);
-    throw err;
+    console.log(err);
+    process.exit(1);
   }
-
-  return cached.conn;
-}
-
-export default connectDB;
+};
